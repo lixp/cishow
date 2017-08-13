7f6461df2e0eed2ade0db98250a18b97773813df
lixp <lixp0915@163.com>
2017-08-13 14:18:19 +0800

    测试用的

---
+++ src/main/java/cn/com/fdc/ci/show/ShowCi.java
@@ -0,0 +1,10 @@
+package cn.com.fdc.ci.show;
+
+/**
+ * Created by po on 17/8/13.
+ */
+public class ShowCi {
+    public static void main(String[] args) {
+        System.out.print("初始化代码");
+    }
+}
